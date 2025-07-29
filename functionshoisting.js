a11e6719f726210e2c7f9a6efee4af7e349f529f
AnkushPille-KF <ankush.pille1@gmail.com>
2025-07-29 16:23:45 +0530

    function expression are not hosited with example

--- functionshoisting.js
+++ functionshoisting.js
@@ -5,3 +5,17 @@ function greet(){
     console.log("hello ankush")
 }
 
+
+//in backend function is hoisted top like below
+//function greet(){
+//console.log("hellow ankush")
+//}
+
+//greet()
+
+//function expression are not hoisted  throws an error cannot acces before delaration
+const namefc = function(){
+    console.log("hello ankush1")
+}
+
+
