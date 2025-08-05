15153f8e897a381cda9ad2c0fb666f719c8c0673
AnkushPille-KF <ankush.pille1@gmail.com>
2025-08-05 20:28:09 +0530

    practice diffrence b/n  var let const

---
+++ interviewquestionspractice/hundredtopics.js
@@ -0,0 +1,40 @@
+
+//var is function scoped let is block scoped const is also block scoped 
+// function testscope(){
+//    if(true){
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+//    }
+//    console.log(a);
+//    console.log(b);
+//    console.log(c);
+// }
+
+// testscope();
+
+// var a = 2;
+// var a = 3;
+// console.log(a);
+
+// //reassign is allowed 2)redecalre is not there for let as it is block scoped
+// let b = 3;
+//  b =4;
+//  console.log(b)
+
+//  //cannot re-assing cannot redeclare
+//  const c = 4;
+//  c =4
+// console.log(c);
+
+//undefined
+// console.log(a);
+// var a = 20;
+
+//tdz cannot access before intialization
+// console.log(b);
+// let b = 30;
+
+//tdz cannot access before intialiazation
+// console.log(c)
+// const c = 40;
