91f37da43f95044d47f2c8e6aeba5d68bc1f4a51
AnkushPille-KF <ankush.pille1@gmail.com>
2025-08-10 16:11:57 +0530

    added middleware to check the user is autorized or not

--- interviewquestionspractice/express.js
+++ interviewquestionspractice/express.js
@@ -4,14 +4,33 @@ const port = 3000;
 
 app.use(express.json());  // Add this line to parse JSON bodies
 
+function authorize(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).json({message: 'unauthorized'})
+    }
+
+   const token = authHeader.split(' ')[1];
+   if(token !== '12345'){
+    return res.status(403).json({message:'forbidden'})
+   }else{
+    next();
+   }
+}
+
 app.get('/', (req,res) => {
     res.send('Hello world');
 })
 
-app.post('/user',(req,res) => {
-    console.log("request is hit")
-    const user = req.body;
-    res.json({ message: 'User created', user: user.name });  // Fixed response format
+app.post('/user', authorize, (req,res) => {
+    try {
+        const user = req.body;
+        // Only reaches this code if authorization was successful
+        console.log("User creation authorized");
+        res.json({ message: 'User created', user: user.name });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating user' });
+    }
 })
 
 app.listen(port,() => {
