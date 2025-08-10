bd989bf0dc7d0435387e04f59e7b9209a00eaf9b
AnkushPille-KF <ankush.pille1@gmail.com>
2025-08-10 15:52:16 +0530

    sample apis nodejs

--- interviewquestionspractice/express.js
+++ interviewquestionspractice/express.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());  // Add this line to parse JSON bodies
+
 app.get('/', (req,res) => {
     res.send('Hello world');
 })
 
 app.post('/user',(req,res) => {
-    res.send('User created');
+    console.log("request is hit")
+    const user = req.body;
+    res.json({ message: 'User created', user: user.name });  // Fixed response format
 })
 
 app.listen(port,() => {
