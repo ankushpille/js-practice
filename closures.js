3bb330dd3019d8e1a9175adc41126428acde53b8
AnkushPille-KF <ankush.pille1@gmail.com>
2025-08-31 22:43:50 +0530

    var settimwout issue fixed using let

--- closures.js
+++ closures.js
@@ -1,13 +1,55 @@
-function outerfunction(){
-    let count =0;
-    function innerfunction(){
-        count = count +1
-        return count
-    }
-    return innerfunction;
+// function outerfunction(){
+//     let count =0;
+//     function innerfunction(){
+//         count = count +1
+//         return count
+//     }
+//     return innerfunction;
+// }
+
+// const example = outerfunction();
+// console.log(example());
+// console.log(example());
+
+// function outerfunction(){
+//     let count = 0;
+//     function innerfunction(){
+//         count = count +1
+//         return count;
+//     }
+//     return innerfunction;
+// }
+
+// const result = outerfunction();
+// console.log(result());
+// console.log(result());
+// console.log(result());
+
+
+// function greet(name,callback){
+//     callback(`hello ${name}`)
+// }
+
+// greet("ankus",console.log)
+
+// function multiply(factor){
+//     return function(num){
+//         return num * factor
+//     }
+// }
+
+// const double = multiply(5);
+// console.log(double(2))
+
+
+console.log("Try programiz.pro");
+
+function test(){
+for(let i=0; i<5; i++){
+    setTimeout(() => {
+         console.log(i)
+    },200)
+}
 }
 
-const example = outerfunction();
-console.log(example());
-console.log(example());
-
+test()
