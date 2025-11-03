cae170b01781cc286cf2a20850a938eea124256e
ankushpille <ankush.pille1@gmail.com>
2025-11-03 11:08:27 +0530

    filtering array using filter method

--- novcoachingpractice.js
+++ novcoachingpractice.js
@@ -1,6 +1,6 @@
 console.log(123.456.toFixed(2));
 
-let salaries = [1000.5,2000.4,3000.5];
+let salaries = [1000.5,2000,3000.5];
 let cl = console.log
 
 let total = salaries.reduce((prevSum,curr) => prevSum + curr,0)
@@ -8,3 +8,6 @@ let total = salaries.reduce((prevSum,curr) => prevSum + curr,0)
 let doubleSalary = salaries.map((num) => num * 2);
 
 cl({doubleSalary});
+
+let greaterThanOrEqualTo2000 = salaries.filter((num) => num > 2000);
+cl({greaterThanOrEqualTo2000});
