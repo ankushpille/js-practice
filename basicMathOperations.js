95208afa0758a17578f7046a9803e99480cf4a12
ankushpille <ankush.pille1@gmail.com>
2025-11-03 14:57:55 +0530

    practicing basic math operators

--- basicMathOperations.js
+++ basicMathOperations.js
@@ -5,3 +5,28 @@ console.log(10%5)
 console.log(2 ** 3)
 
 
+//Math.floor
+console.log("Math method",Math.floor(4.2))
+console.log("Math.floor",Math.floor(4.6));
+
+//Math.round
+console.log("Math.round",Math.round(4.9));
+console.log("Math.round",Math.round(4.1))
+console.log("Math.round",Math.round(4.5))
+
+//Math.ceil
+console.log("math.ceil",Math.ceil(4.2))
+console.log("Math.ceil",Math.ceil(4.1))
+
+//Math.random
+console.log("Math.random",Math.random())
+
+console.log("Math.sqrt",Math.sqrt(4))
+console.log("Math.pow",Math.pow(2,4));
+
+
+console.log((123.456).toFixed(1));
+console.log(parseInt('10'));
+console.log(parseInt('10.5'))
+
+
