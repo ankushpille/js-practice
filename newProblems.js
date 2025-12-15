b1315950f6e3e3a46d15951fd6fb4bbeaa31e7b7
ankushpille <ankush.pille1@gmail.com>
2025-12-15 22:41:25 +0530

    simple array operation

--- newProblems.js
+++ newProblems.js
@@ -2,3 +2,10 @@ const fruits = ["apple","bannana","mango"]
 fruits.shift("");
 fruits.push("orange");
 console.log(fruits);
+
+const numbers = [2, 4, 6, 8, 10];
+const doubledArray = numbers.map((num) => {
+    return num*2
+})
+
+console.log(doubledArray);
