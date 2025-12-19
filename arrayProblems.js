7ed62785d603ebabaad74aee553720a148d5d8ee
ankushpille <ankush.pille1@gmail.com>
2025-12-19 14:09:55 +0530

    sum of all elements

--- arrayProblems.js
+++ arrayProblems.js
@@ -18,3 +18,12 @@ for(let i=0; i<arr.length; i++){
         console.log("even number",arr[i])
     }
 }
+
+
+arr = [2, 4, 6, 8]
+let sum = 0;
+for(let i=0; i<arr.length; i++){
+    sum = sum + arr[i];
+}
+
+console.log("sum",sum)
