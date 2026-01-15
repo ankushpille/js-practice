e7cfe193070e3cc03531c0207a3690743d0ed412
ankushpille <ankush.pille1@gmail.com>
2026-01-15 19:31:04 +0530

    refactor

---
+++ leetcodeproblems.js
@@ -0,0 +1,24 @@
+var fizzBuzz = function (n) {
+  let res = [];
+
+  for (let i = 1; i <= n; i++) {
+    let s = "";
+
+    if (i % 3 === 0) {
+      s += "Fizz";
+    }
+    if (i % 5 === 0) {
+      s += "Buzz";
+    }
+
+    if (s === "") {
+      s = "" + i;
+    }
+
+    res.push(s);
+  }
+
+  return res;
+};
+
+console.log(fizzBuzz(3));
