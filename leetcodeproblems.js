3fe2f11664beca27fbdd2cf1f95a22a04fced590
ankushpille <ankush.pille1@gmail.com>
2026-01-15 19:53:23 +0530

    reverse integer

--- leetcodeproblems.js
+++ leetcodeproblems.js
@@ -22,3 +22,21 @@ var fizzBuzz = function (n) {
 };
 
 console.log(fizzBuzz(3));
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  let rev = 0;
+
+  while (x !== 0) {
+    let rem = x % 10;
+    rev = rev * 10 + rem;
+    x = Math.trunc(x / 10);
+  }
+  let = [Low, High] = [-(2 ** 31), 2 ** 31 - 1];
+  if (rev < Low) return 0;
+  if (rev > High) return 0;
+  return rev;
+};
