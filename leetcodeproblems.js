20259ce2146254c727892fb706d2157f38a2d52f
ankushpille <ankush.pille1@gmail.com>
2026-01-30 13:43:17 +0530

    stack class

--- leetcodeproblems.js
+++ leetcodeproblems.js
@@ -864,3 +864,28 @@ var tribonacci22 = function (n) {
 
   return next;
 };
+
+//how to write stack
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(element) {
+    this.items.push(element);
+  }
+  pop() {
+    this.items.pop();
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
+  printStack() {
+    console.log(this.items.toString());
+  }
+}
