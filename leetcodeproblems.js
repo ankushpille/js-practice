3b87cad0f54787faa1748027ad4914bc754275c2
ankushpille <ankush.pille1@gmail.com>
2026-02-03 22:29:11 +0530

    best time to buy and sell stock

--- leetcodeproblems.js
+++ leetcodeproblems.js
@@ -1016,3 +1016,21 @@ class Queue_ {
     console.log(this.arr);
   }
 }
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+     let min=prices[0];
+     let maxprofit = 0;
+     for(let i=1; i<prices.length; i++){
+        if(prices[i]-min > maxprofit){
+            maxprofit = prices[i] - min
+        }
+        if(prices[i] < min){
+            min = prices[i]
+        }
+     }
+     return maxprofit;
+};
