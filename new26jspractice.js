a51a90b9c2fefa3cad62b1882da7b3ffeb841172
ankushpille <ankush.pille1@gmail.com>
2026-03-11 16:35:12 +0530

    prototype example

--- new26jspractice.js
+++ new26jspractice.js
@@ -117,3 +117,14 @@ const counter = outerFunction();
 counter(); // Output: "Count: 1"
 counter();
 counter(); // Output: "Count: 3"
+
+// prototype example
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+const person1 = new Person("Alice", 30);
+person1.greet(); // Output: "Hello, my name is Alice and I am 30 years old."
